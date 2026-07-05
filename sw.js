a42b0c22a02e3f2e2a1d3937dffcf9b954335a3d
netramverma <netramgedar1212@gmail.com>
2026-07-05 19:43:58 +0530

    Initialize service worker with fetch event listener
    Add basic service worker setup for fetch events.

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Is file ko aap khali (blank) bhi chhod sakte hain, ya ye basic code dal sakte hain
+self.addEventListener('fetch', function(event) {
+    // App ko background mein chalaye rakhne ke liye
+});
